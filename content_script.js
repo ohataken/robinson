dece8436ea749665fda81d3a4077aaedcb794572
ohataken <ohataken@outlook.com>
2017-01-23 21:55:12 +0900

    support clipboard copy for chrome

--- content_script.js
+++ content_script.js
@@ -1,3 +1,9 @@
 browser.runtime.onMessage.addListener((message) => {
   alert(message.link);
+  const el = document.createElement("input");
+  document.body.appendChild(el);
+  el.setAttribute("type", "text");
+  el.setAttribute("value", message.link);
+  el.select();
+  document.execCommand("copy");
 });
